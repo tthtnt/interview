1df0acd30958d6c2d4b40336a3e18e39607c9224
v_isijieliu <v_isijieliu@tencent.com>
2023-09-09 21:33:01 +0800

    load和DOMContentLoaded区别

--- webpack/dist/d.js
+++ webpack/dist/d.js
@@ -1,2 +1,11 @@
 console.log('ddd')
 console.log('zzz')
+
+
+window.addEventListener('load', function () {
+    // 页面的全部资源加载完才会执行，包括图片，视频等
+})
+
+window.addEventListener('DOMContentLoaded', function () {
+    // DOM 渲染完即可执行，此时图片、视频可能还没有加载完
+})
